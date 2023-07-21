f9c15e209161c4c02602a14d6c29ad238920b205
ptan01 <ptanjim01@gmail.com>
2023-07-22 01:24:39 +0600

    set tailwind and deasyui , install react router and create leyout

---
+++ src/sharedSection/Footer.jsx
@@ -0,0 +1,10 @@
+
+const Footer = () => {
+    return (
+        <div>
+            <h1>This is footers</h1>
+        </div>
+    );
+};
+
+export default Footer;
---
+++ src/sharedSection/Navbar.jsx
@@ -0,0 +1,10 @@
+
+const Navbar = () => {
+    return (
+        <div>
+            <h1 className="text-red-700">this is navbar</h1>
+        </div>
+    );
+};
+
+export default Navbar;
