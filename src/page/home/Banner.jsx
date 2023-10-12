13d7c8213c029d307a1b333635f6e8656520714b
ptan01 <ptanjim01@gmail.com>
2023-10-12 18:48:09 +0600

    banner changes

--- src/page/home/Banner.jsx
+++ src/page/home/Banner.jsx
@@ -1,57 +1,15 @@
-import "./Banner.css"
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import { Navigation } from 'swiper/modules';
-
-
 
 
 const Banner = () => {
     return (
-        <div className="text-gray-100">
-            <Swiper navigation={true} modules={[Navigation]} effect="EffectFade" className="mySwiper">
-                <SwiperSlide>
-                    <div className="bg-img1 h-[100vh]">
-                        <div className="flex flex-col justify-center items-center h-[100vh] ">
-                            <h1 className="text-4xl font-bold">Unlock Your Future: College Booking Made Easy</h1>
-                            <p className="text-xl font-semibold">Empowering Students to Choose their Path to Success</p>
-                        </div>
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <div className="bg-img2 h-[100vh]">
-                        <div className="flex flex-col justify-center items-center h-[100vh] ">
-                            <h1 className="text-4xl font-bold">Find and Book Your Dream College</h1>
-                            <p className="text-xl font-semibold">Your Journey to Higher Education Starts Here</p>
-                        </div>
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <div className="bg-img3 h-[100vh]">
-                        <div className="flex flex-col justify-center items-center h-[100vh] ">
-                            <h1 className="text-4xl font-bold">Find and Book Your Dream College</h1>
-                            <p className="text-xl font-semibold">Explore, Compare, and Reserve Your Ideal College</p>
-                        </div>
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <div className="bg-img4 h-[100vh]">
-                        <div className="flex flex-col justify-center items-center h-[100vh] ">
-                            <h1 className="text-4xl font-bold">Unlock Your Future: College Booking Made Easy</h1>
-                            <p className="text-xl font-semibold">Empowering Students to Choose their Path to Success</p>
-                        </div>
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <div className="bg-img5 h-[100vh]">
-                        <div className="flex flex-col justify-center items-center h-[100vh] ">
-                            <h1 className="text-4xl font-bold">Find and Book Your Dream College</h1>
-                            <p className="text-xl font-semibold">Your Journey to Higher Education Starts Here</p>
-                        </div>
-                    </div>
-                </SwiperSlide>
-            </Swiper>
+        <div className="lg:flex justify-between items-center h-[50vh]">
+            <div className="lg:w-1/2">
+                <h1 className="text-5xl font-bold">A World of Opportunities <br /> Awaits You</h1>
+                <p>We Believe in Your Potential: Campus Wise Offers a Supportive Environment, World-Class Facilities, and a Vibrant Campus Culture to Help You Achieve Your Educational and Career Goals.</p>
+            </div>
+            <div>
+                <img className="rounded-lg" src="https://st3.depositphotos.com/4678277/16129/i/450/depositphotos_161299614-stock-photo-its-more-easy-together-six.jpg" alt="bannar img" />
+            </div>
 
         </div>
     );
